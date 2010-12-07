c57b4eb63bd97dba8b5a08e750899e0e2ad083ee
Aaron Ryan <halfeatencake@gmail.com>
2010-12-07 01:13:50 -0500

    Started to add more of a menu to theproject.

--- ASEmbed.py
+++ ASEmbed.py
@@ -9,17 +9,38 @@ from ASBuilder import *
 
 class LevelEditor:
     def __init__(self, master):
+        #Create the frame's for the different sections of the main window
     	self.windowFrame = Frame(root)
+    	self.compilerFrame = Frame(root)
+    	self.buildTypeFrame = Frame(root)
+
+        #Create variables to store current selections.
+        self.compileType = "SWC"
+    	self.buildType = StringVar()
+    	self.buildType.set("Image")
     	
+    	#Build the GUI
     	self.buildWindow()
     	
+    	
     def buildWindow(self):
+        #Create all the buttons
         newButton(self.windowFrame, "Build BitmapData", self.buildBitmapData, 0,0)
         newButton(self.windowFrame, "Build Sprite", self.buildSprite, 1,0)
         newButton(self.windowFrame, "Build XML", self.buildXML, 2,0)
         
-        self.windowFrame.grid(row=0, column = 0)
-    
+        #Still needs to do some sort of action. Need to set default
+        Radiobutton(self.compilerFrame, text="SWF", variable=self.compileType, value="SWF").grid(row=0, column=0)
+        Radiobutton(self.compilerFrame, text="SWC", variable=self.compileType, value="SWC").grid(row=0, column=1)
+
+        #Create the menu of what type of libraries can be created.
+        OptionMenu(self.buildTypeFrame, self.buildType, "Image", "XML", "Audio").pack(anchor=W)
+
+        self.compilerFrame.grid(row=0, column=0)
+        self.windowFrame.grid(row=1, column = 0)
+        self.buildTypeFrame.grid(row=2, column=0)
+
+
     def buildBitmapData(self):
         directory = tkFileDialog.askdirectory()
         generateASBitmapFiles(directory, directory)
@@ -27,12 +48,14 @@ class LevelEditor:
         flexBuild(directory)
         removeASFiles(directory)
         
+        
     def buildSprite(self):
         directory = tkFileDialog.askdirectory()
         generateASSpriteFiles(directory, directory)
         buildLinkerASFile(directory, directory)
         flexBuild(directory)
         removeASFiles(directory)
+        
         
     def buildXML(self):
         directory = tkFileDialog.askdirectory()
@@ -45,7 +68,7 @@ class LevelEditor:
         
 root = Tk()
 root.title('ASEmbed')
-root.geometry("%dx%d%+d%+d" % (250, 150, 0, 0))
+root.geometry("%dx%d%+d%+d" % (350, 250, 0, 0))
 
 app = LevelEditor(root)
 root.mainloop()
