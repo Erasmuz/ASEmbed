cb9f862602970854bd7db5b9157c943af433a057
AaronRyan <none@none>
2010-12-07 23:16:28 -0800

    Added items needed to build in windows.

---
+++ source/buildWindows.bat
@@ -0,0 +1,30 @@
+REM Configure the installer
+
+
+cd pyinstaller-1.5
+
+c:\python24\python.exe Configure.py
+
+
+
+REM Used to generate initial makespec, but then modified to keep everything local
+
+REM c:\python24\python.exe Makespec.py -K --paths=../helper --out=../../binaries --onefile ../ASEmbed.py
+
+
+
+REM Build the binaries
+
+c:\python24\python.exe Build.py ../../binaries/ASEmbed.spec
+
+
+
+#Clean up
+
+cd ../../binaries
+
+move dist\ASEmbed.exe
+rmdir /S /Q build
+
+rmdir /S /Q dist
+cd ../source
